e1db376cfb744e25233e409772eb59822d75aeb2
fanofrisoni <fanofrisoni4@gmail.com>
2023-12-18 14:15:28 -0600

    Add homepage

--- frontend_rf/src/pages/pageHome.jsx
+++ frontend_rf/src/pages/pageHome.jsx
@@ -4,11 +4,8 @@ import ImageHome from "../components/ImageHome/ImageHome";
 import { useNavigate } from "react-router-dom";
 function PageHome() {
   let navigate = useNavigate();
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 6380a50ce44bd4959e1866204d185fc30955ff14
   return (
    
       <ImageHome/>
--- frontend_rf/src/pages/pageProfile.jsx
+++ frontend_rf/src/pages/pageProfile.jsx
@@ -10,11 +10,17 @@ import { Link, useParams } from "react-router-dom";
 import ButtonProfile from "../components/Button/ButtonProfile";
 import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
+import {
+  InputPassShort,
+  InputTelShort,
+} from "../components/InputText/InputText";
 function PageProfile({ userLoggedIn, setUserLoggedIn }) {
   let navigate = useNavigate();
   const { userId } = useParams();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [tel, setTel] = useState("");
+  const [pass, setPass] = useState("");
 
   const handleLogOut = () => {
     localStorage.clear();
@@ -23,7 +29,7 @@ function PageProfile({ userLoggedIn, setUserLoggedIn }) {
   };
   useEffect(() => {
     if (!userLoggedIn) {
-      navigate("/signup");
+      //navigate("/signup");
     }
   });
   return (
@@ -75,7 +81,7 @@ function PageProfile({ userLoggedIn, setUserLoggedIn }) {
           style={{
             marginTop: "2vh",
             fontFamily: "PoppinsMedium",
-            fontWeight: "800",
+            fontWeight: "300",
             fontSize: "14px",
             textAlign: "left",
             marginBottom: "10px",
@@ -83,7 +89,21 @@ function PageProfile({ userLoggedIn, setUserLoggedIn }) {
             gridColumn: "span 1",
           }}
         >
-          nombre
+          Stefano
+        </h2>
+        <h2
+          style={{
+            marginTop: "2vh",
+            fontFamily: "PoppinsMedium",
+            fontWeight: "300",
+            fontSize: "14px",
+            textAlign: "left",
+            marginBottom: "10px",
+            color: mainColors.textBlack,
+            gridColumn: "span 1",
+          }}
+        >
+          Frisoni
         </h2>
         <h2
           style={{
@@ -97,11 +117,36 @@ function PageProfile({ userLoggedIn, setUserLoggedIn }) {
             gridColumn: "span 1",
           }}
         >
-          apellido
+          Telefono
         </h2>
+        <h2
+          style={{
+            marginTop: "2vh",
+            fontFamily: "PoppinsMedium",
+            fontWeight: "300",
+            fontSize: "14px",
+            textAlign: "left",
+            marginBottom: "10px",
+            color: mainColors.textBlack,
+            gridColumn: "span 1",
+          }}
+        >
+          6141694297
+        </h2>
+        <InputTelShort tel={tel} setTel={setTel} />
+
         <ButtonProfile
           text={"Cambiar"}
-          click={() => console.log("Cambiar nombre")}
+          click={() => console.log("Cambiar telefono")}
+        />
+        <InputPassShort pass={pass} setPass={setPass} />
+        <ButtonProfile
+          text={"Cambiar"}
+          click={() => console.log("Cambiar contrasena")}
+        />
+        <Button
+          text={"Mis reservas"}
+          click={() => navigate(`/reservations/${userId}`)}
         />
         <Button text={"Cerrar sesión"} click={handleLogOut} />
       </LayoutGrid>
