ef7693c2ee246d7eac0d68e2ce52da77d43c96d5
fanofrisoni <fanofrisoni4@gmail.com>
2023-12-18 14:39:20 -0600

    Fix route home reseve

--- frontend_rf/src/components/ImageHome/ImageHome.jsx
+++ frontend_rf/src/components/ImageHome/ImageHome.jsx
@@ -10,160 +10,143 @@ import { useNavigate } from "react-router-dom";
 function ImageHome({ size = "0" }) {
   let navigate = useNavigate();
   return (
-    <Link
-      to={"/home"}
-      style={{ textDecoration: "none", color: mainColors.textBlack }}
-    >
-       <div style={{
+    <div
+      style={{
         display: "flex",
-        height: "70vh",
+        height: "100vh",
         width: "100%",
         flexDirection: "column",
         justifyContent: "flex-start",
         alignItems: "center",
         gap: "10px",
-      }}>
-        <img
-          src={imagenRestaurant}
-          alt="imagenRestaurant"
-          style={{
-            width: "100%",
-            height: "100%",
-            zIndex: -1,
-            objectFit: "cover",
-            position: "absolute",
-            margin: 0,
-          }}
-        />
-        <img
-          src={LogoEpicureos}
-          alt="Epicureos"
-          style={{
-            zIndex: 3,
-            top: "50px",
-          }}
-        />
-          
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+          position: "absolute",
+          backgroundColor: mainColors.backBrownOpaque2,
+        }}
+      ></div>
+      <div style={{ paddingTop: "25px" }}></div>
+      <img
+        src={imagenRestaurant}
+        alt="imagenRestaurant"
+        style={{
+          width: "100%",
+          height: "100%",
+          zIndex: -2,
+          objectFit: "cover",
+          position: "absolute",
+        }}
+      />
+      <img src={LogoEpicureos} alt="Epicureos" />
 
-        <h2
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "20px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-            textAlign: "left",
-          }}
-        >
-          MERECEDORES DE PLACER
-        </h2>
-        <h3
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "11px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-          }}
-        >
-          Deleitá tu paladar con nuestra propuesta gastronómica
-        </h3>
-        <h4
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "10px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-          }}
-        >
-          Lennox 2308, Funes
-        </h4>
-       
-        <h5
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "15px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-          }}
-        >
-          NUESTROS HORARIOS:
+      <h2
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "20px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+          textAlign: "left",
+        }}
+      >
+        MERECEDORES DE PLACER
+      </h2>
+      <h3
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "11px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        Deleitá tu paladar con nuestra propuesta gastronómica
+      </h3>
+      <h4
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "10px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        Lennox 2308, Funes
+      </h4>
 
-        </h5>
+      <h5
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "15px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        NUESTROS HORARIOS:
+      </h5>
 
-        <h6
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "11px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-          }}
-        >
-          Martes a Jueves: De 12 a 16 y de 19 a 00:30hs
+      <h6
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "11px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        Martes a Jueves: De 12 a 16 y de 19 a 00:30hs
+      </h6>
 
-        </h6>
+      <h7
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "11px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        Viernes y Sábados: De 12 a 16 y de 19 a 01:30hs
+      </h7>
+      <h8
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "11px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+          alignItems: "left",
+        }}
+      >
+        Domingos: De 12 a 16 y de 19 a 00:30hs
+      </h8>
+      <h9
+        style={{
+          fontFamily: "PoppinsMedium",
+          fontWeight: "400",
+          fontSize: "11px",
+          marginBottom: "0px",
+          color: mainColors.textWhite,
+        }}
+      >
+        Lunes: Cerrado
+      </h9>
 
-        <h7
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "11px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-          }}
-        >
-          Viernes y Sábados: De 12 a 16 y de 19 a 01:30hs
-
-        </h7>
-        <h8
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "11px",
-            marginBottom: "0px",
-            color: mainColors.textWhite,
-            alignItems: "left",
-          }}
-        >
-          Domingos: De 12 a 16 y de 19 a 00:30hs
-
-        </h8>
-        <h9
-          style={{
-            fontFamily: "PoppinsMedium",
-            fontWeight: "400",
-            fontSize: "11px",
-            marginBottom: "0px",
-            color: mainColors.textWhite, 
-
-          }}
-        >
-          Lunes: Cerrado
-
-        </h9>
- 
-
-        <Button
-          loading={false}
-          text={"Reserva"}
-          click={() => navigate("/reserve")}
-          style={{
-            position: "absolute",
-            top: "80px",
-            left: "50%",
-            width: "50px",
-            transform: "translate(-50%, -50%)",
-            zIndex: 2,
-          }}
-        />
-        
-        
-
-      </div>
-    </Link>
+      <Button
+        loading={false}
+        text={"Reserva"}
+        click={() => navigate("/reserve")}
+        style={{
+          width: "50px",
+        }}
+      />
+    </div>
   );
 }
 
