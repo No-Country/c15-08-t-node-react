f75ea6a568ddd85439204b70636d3414861546d1
AngelBlackBlue <angel.blackbkue@hotmail.com>
2024-01-30 13:14:29 -0300

    node cron en segund plano

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,8 +1,38 @@
 const { Router } = require('express')
-
+const cron = require('node-cron');
+const { 
+    createAvailabilityDates, 
+    disablePreviousDates } = require('../controllers/availability.controller');
 
 
 const router =  Router()
+
+cron.schedule('0 14 * * *', async () => {
+  console.log('Ejecutando tarea cron createAvailabilityDates');
+  try {
+    await createAvailabilityDates();
+    console.log('Tarea cron createAvailabilityDates completada con éxito');
+  } catch (error) {
+    console.error('Error en la tarea cron createAvailabilityDates:', error);
+  }
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
+cron.schedule('1 0 * * *', async () => {
+  console.log('Ejecutando tarea cron disablePreviousDates');
+  try {
+    await disablePreviousDates();
+    console.log('Tarea cron disablePreviousDates completada con éxito');
+  } catch (error) {
+    console.error('Error en la tarea cron disablePreviousDates:', error);
+  }
+}, {
+  scheduled: true,
+  timezone: "America/Sao_Paulo"
+});
+
 
 router.use('/auth', require('./auth.routes'))
 router.use('/user', require('./user.routes'))
